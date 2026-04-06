245f14eb4e753a418e8a7ae94ae08e663a3c94b3
Bilal Ahmad <bilal.codemaster@gmail.com>
2026-04-06 15:03:58 +0500

    done

--- app/shared/Header.tsx
+++ app/shared/Header.tsx
@@ -36,15 +36,17 @@ export const Header = () => {
   return (
     <>
       <nav className="fixed w-full top-0 z-[100] border-b border-slate-100 bg-white/90  shadow-sm">
-        <div className="container-wrap px-4 md:px-6 lg:px-14 py-3 md:py-4 flex items-center justify-between gap-4">
+        <div className="container-wrap px-4 md:px-6 lg:px-[93.8px] py-3 md:py-4 flex items-center justify-between gap-4">
 
           {/* Logo Section */}
-          <Link href="/" className="flex items-start text-lg md:text-3xl font-black  uppercase text-slate-900 z-[110]">
-            <img src="https://www.alrashideenengg.com/img/logo.alrashideen.png" alt="Logo" className="w-12 h-12 md:w-20 md:h-20 mr-2 -mt-1 md:-mt-4" />
+          <Link href="/" className="flex items-start text-xl md:text-2xl font-black tracking-tighter uppercase text-slate-900 z-[110]">
+
+            <img src="https://www.alrashideenengg.com/img/logo.alrashideen.png" alt="Logo" className="w-20 h-20 mr-2 -mt-4 self-start" />
             <div className="flex flex-col leading-tight">
               AL RASHIDEEN
-              <span className="text-[10px] md:text-xl font-bold">ENGINEERING TURNING</span>
+              <span className="text-lg md:text-xl">ENGINEERING TURNING</span>
             </div>
+
           </Link>
 
           {/* Desktop Navigation */}
@@ -55,9 +57,9 @@ export const Header = () => {
                 <Link
                   key={link.name}
                   href={link.href}
-                  className={`text-sm tracking-tight transition-all duration-200 ${isActive
+                  className={`text-[14px] tracking-tight    pb-1   ${isActive
                     ? "text-yellow-600 border-b-2 border-yellow-500 pb-1 font-bold"
-                    : "text-slate-600 hover:text-slate-900 font-medium"
+                    : "text-slate-600 hover:text-slate-900  font-bold"
                     }`}
                 >
                   {link.name}
@@ -68,7 +70,7 @@ export const Header = () => {
 
           {/* Action Buttons & Hamburger */}
           <div className="flex items-center gap-2 md:gap-3">
-            <div className="hidden sm:flex items-center gap-2 md:gap-3">
+            <div className="hidden relative right-7 sm:flex items-center gap-2 md:gap-3">
               <Link href="/contact#inquiry" className="px-4 py-2 rounded-lg bg-[#ffd700] text-[#221b00] text-sm font-bold hover:bg-[#e6c200] transition-colors">Enquiry</Link>
               <Link href="/contact" className="px-4 py-2 rounded-lg border border-[#d0c6ab] text-sm font-bold text-[#181a30]">Contact</Link>
             </div>
